b2dcb43474d6e80a84eae3471775c731d0bc41cf
Santosh Kumar <kasantosh@yahoo.com>
2020-03-27 13:56:02 -0400

    local stoarge for location works, project complete

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 // Init weather object
-const weather = new Weather('Mississauga', 'ON');
+// Init storage
+const storage = new Storage();
+// Get stored location data
+const weatherLocation = storage.getLocationData();
+
+// Init weather
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
 
 // Init UI
 const ui = new UI();
@@ -12,7 +18,14 @@ document.getElementById('w-change-btn').addEventListener('click', (e) => {
   const city = document.getElementById('city').value;
   const state = document.getElementById('state').value;
 
+  // Change location
   weather.changeLocation(city, state);
+
+  // Set location in LS
+  if (city !== '' && state !== '') {
+    storage.setLocationData(city, state);
+  }
+
 
   // Get and display Weather
   getWeather();
@@ -26,7 +39,6 @@ document.getElementById('w-change-btn').addEventListener('click', (e) => {
 function getWeather() {
   weather.getWeather()
     .then(results => {
-      console.log(results.current.temperature);
       ui.paint(results);
     })
     .catch(err => console.log(err));
