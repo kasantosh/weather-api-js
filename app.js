04f1389e89224fb802329fa826882d48f8b80c24
Santosh Kumar <kasantosh@yahoo.com>
2020-03-27 13:31:20 -0400

    Able to display the weather in the UI and also for changes

--- app.js
+++ app.js
@@ -5,7 +5,23 @@ const weather = new Weather('Mississauga', 'ON');
 const ui = new UI();
 
 // Get weather on DOM load
-document.addEventListener('DOMContentLoaded', getWeather)
+document.addEventListener('DOMContentLoaded', getWeather);
+
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+  const state = document.getElementById('state').value;
+
+  weather.changeLocation(city, state);
+
+  // Get and display Weather
+  getWeather();
+
+  // Close modal
+  $('#locModal').modal('hide');
+
+
+});
 
 function getWeather() {
   weather.getWeather()
--- ui.js
+++ ui.js
@@ -9,17 +9,20 @@ class UI {
     this.feelsLike = document.getElementById('w-feels-like');
     this.uvindex = document.getElementById('w-uv-index');
     this.wind = document.getElementById('w-wind');
+    this.fdegree = document.getElementById('f-text')
 
   }
 
   paint(weather) {
     this.location.textContent = `${weather.location.name} ${weather.location.region}, ${weather.location.country}`;
     this.desc.textContent = weather.current.weather_descriptions[0];
-    this.string.textContent = `${weather.current.temperature}	&#176C`;
+    this.string.textContent = weather.current.temperature;
     this.icon.setAttribute('src', weather.current.weather_icons[0]);
     this.humidity.textContent = `Relative Humidity: ${weather.current.humidity}`;
     this.feelsLike.textContent = `Feels Like: ${weather.current.feelslike}`;
     this.uvindex.textContent = `UV Index: ${weather.current.uv_index}`;
     this.wind.textContent = `Wind Speed: ${weather.current.wind_speed}`;
+    const fdeg = (parseInt(weather.current.temperature) * 9 / 5) + 32;
+    this.fdegree.textContent = `(${fdeg}F)`
   }
 }
